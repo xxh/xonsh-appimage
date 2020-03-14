65cf15b12a075e364d2793282690168ab80ce243
anki-code <anki-code>
2020-03-14 10:51:20 +0300

    0.5.8

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t xonssh/xonsh-appimage
-docker run --rm -v -it --privileged --device /dev/fuse -v `pwd`/result:/result xonssh/xonsh-appimage
+docker build . -t xxh/xonsh-appimage
+docker run --rm -v -it --privileged --device /dev/fuse -v `pwd`/result:/result xxh/xonsh-appimage
