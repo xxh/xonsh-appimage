9d3cc11dff33db7a8ecd645b93e06a30aa13a0ee
anki-code <anki-code>
2020-04-19 11:22:30 +0300

    build

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/niess/python-appimage
 cd python-appimage
 python -m python_appimage build app applications/xonsh
-cp xonsh-x86_64.AppImage result/
+cp xonsh-x86_64.AppImage ../result/
+cd .. && rm -rf python-appimage
