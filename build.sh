e79a0f929b6f853e84bc1c74790a1e346c8e48b1
anki-code <anki-code>
2020-04-19 11:31:23 +0300

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 
 git clone https://github.com/niess/python-appimage
 cd python-appimage
+
+echo Build xonsh release
 python -m python_appimage build app applications/xonsh
 cp xonsh-x86_64.AppImage ../result/
+
+echo Build xonsh 0.9.13
+echo -e "xonsh==0.9.13\nprompt_toolkit\nPygments" > applications/xonsh/requirements.txt
+python -m python_appimage build app applications/xonsh
+cp xonsh-x86_64.AppImage ../result/xonsh-0.9.13-x86_64.AppImage
+
 cd .. && rm -rf python-appimage
